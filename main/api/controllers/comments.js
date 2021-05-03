d6096ebc9fa9949dda3c34d0c6fbe53d9ee0a318
casder-succ <casderiopus@gmail.com>
2021-05-03 23:13:25 +0300

    Add array's autogenerating

--- main/api/controllers/comments.js
+++ main/api/controllers/comments.js
@@ -13,27 +13,33 @@ module.exports = {
 
 function getComments(req, res) {
 
-    const getDate = function() {
-      const years = [2019, 2020, 2021];
+  const getDate = function() {
+    const years = [2019, 2020, 2021];
 
-      let day = Math.floor(Math.random() * 30 + 1);
-      let month = Math.floor(Math.random() * 11 + 1);
-      let year = years[Math.floor(Math.random() * 3)];
+    let day = Math.floor(Math.random() * 30 + 1);
+    let month = Math.floor(Math.random() * 11 + 1);
+    let year = years[Math.floor(Math.random() * 3)];
 
-      if (day < 10) day = `0${day}`;
-      if (month < 10) month = `0${month}`;
+    if (day < 10) day = `0${day}`;
+    if (month < 10) month = `0${month}`;
 
-      return `${day}-${month}-${year}`;
-    };
+    return `${day}-${month}-${year}`;
+  };
 
-  res.json([
-    {
+  const dataRes = [];
+
+  for (let i = 0; i < Math.floor(Math.random() * 20 + 1); i += 1) {
+    dataRes.push(
+      {
         id:        '' + faker.datatype.number(),
         firstName: '' + faker.name.firstName(),
         lastName:  '' + faker.name.lastName(),
         comment:   '' + faker.lorem.paragraph(),
         date:      '' + getDate(),
         image:     '' + faker.image.avatar(),
-    }
-  ]);
+      },
+    );
+  }
+
+  res.json([...dataRes]);
 }
