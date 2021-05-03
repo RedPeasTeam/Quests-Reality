903b9dc0d16f4a3e926690b3aee09eb7a572b4d7
casder-succ <casderiopus@gmail.com>
2021-05-03 20:49:28 +0300

    Edit swagger desc and fix som problems

--- main/api/controllers/comments.js
+++ main/api/controllers/comments.js
@@ -13,43 +13,27 @@ module.exports = {
 
 function getComments(req, res) {
 
-    /*var schema = [{
-        type: 'array',
-        items: {
-            type: 'Object',
-            properties: {
-                id: {
-                    type: 'number',
-                },
-                name: {
-                    type: 'string',
-                    faker: 'name.firstName',
-                },
-                comment: {
-                    "type": "string",
-                    "faker": "lorem.text",
-                },
-                date: {
-                    type: 'string',
-                    faker: 'date.recent',
-                },
-                image: {
-                    type: 'string',
-                    faker: 'image.avatar',
-                }
-            }
-        },
-        "minItems": 2,
-        "maxItems": 10,
-    }];*/
+    const getDate = function() {
+      const years = [2019, 2020, 2021];
+
+      let day = Math.floor(Math.random() * 30 + 1);
+      let month = Math.floor(Math.random() * 11 + 1);
+      let year = years[Math.floor(Math.random() * 3)];
+
+      if (day < 10) day = `0${day}`;
+      if (month < 10) month = `0${month}`;
+
+      return `${day}-${month}-${year}`;
+    };
 
   res.json([
     {
-        id: 'datatype.number',
-        name: "name.firstName",
-        comment: "lorem.paragraph",
-        date: "date.recent",
-        image: "image.avatar",
+        id:        '' + faker.datatype.number(),
+        firstName: '' + faker.name.firstName(),
+        lastName:  '' + faker.name.lastName(),
+        comment:   '' + faker.lorem.paragraph(),
+        date:      '' + getDate(),
+        image:     '' + faker.image.avatar(),
     }
   ]);
 }
