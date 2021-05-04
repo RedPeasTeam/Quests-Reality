f659b149a1588f35b4646dc35b12459631a5b97b
Kseniya Blagireva <ksenia062003@gmail.com>
2021-05-04 19:24:15 +0300

    Ksuha:add main pages

--- quest-react-app/src/components/mainPages/questLocation/QuestLocation.js
+++ quest-react-app/src/components/mainPages/questLocation/QuestLocation.js
@@ -1,8 +1,19 @@
 import React, {Component} from 'react';
+import { YMaps, Map } from "react-yandex-maps";
+
+const mapData = {
+    center: [53.9254459, 27.5942589],
+    zoom: 10,
+};
 
 class QuestLocation extends Component {
     render() {
-        return <div></div>
+        return <div className='quest-location'>
+        <div className='title'>Quest location</div>
+        <YMaps>
+            <Map defaultState={mapData} width='800px' height='600px' class='map' />
+        </YMaps>
+    </div>
     }
 }
 
