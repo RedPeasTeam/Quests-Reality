e2dd1c844ae99c5b57ff81fc56bb62830a98f80e
casder-succ <casderiopus@gmail.com>
2021-05-04 22:33:55 +0300

    Add new component

--- quest-react-app/src/components/mainPages/reviews/Reviews.js
+++ quest-react-app/src/components/mainPages/reviews/Reviews.js
@@ -1,8 +1,32 @@
 import React, {Component} from 'react';
+import * as Api from 'typescript-fetch-api'
 
+const api = new Api.DefaultApi()
 class Reviews extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {comments: []};
+        this.handleReload()
+     }
+
+     async handleReload() {
+        const response = await api.comments({ date: '' });
+        this.setState({ comments: response });
+     }
+
     render() {
-        return <div></div>
+        return <div className='home'>
+            <div className='title'>Reviews</div>
+            <div className='reviews-container'>
+            {this.state.comments.map(
+                (element) => <div className='reviews-item'>
+                    <img src={element.image}></img>
+                    <h4>{element.firstName} {element.lastName}</h4>
+                    <p>{element.comment}</p>
+                    <p>{element.date}</p>
+                </div>)}
+            </div>
+        </div>
     }
 }
 
