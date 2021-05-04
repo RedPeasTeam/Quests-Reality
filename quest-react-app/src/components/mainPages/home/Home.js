7e0f0a6cf9def477c052436c6c9f8ac39585fea5
vladdominator <https://github.com/vladdominator>
2021-05-04 21:25:13 +0300

    add main js

--- quest-react-app/src/components/mainPages/home/Home.js
+++ quest-react-app/src/components/mainPages/home/Home.js
@@ -1,19 +1,24 @@
 import React, {Component} from 'react';
-import ImgBlock from "../../helpers/imgBlock/ImgBlock";
+import * as Api from 'typescript-fetch-api'
+// import ImgBlock from "../../helpers/imgBlock/ImgBlock";
 
 //const imgs = ['../../../img/testImg.png'];
-
+const api = new Api.DefaultApi()
 class Home extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {main: []};
+      this.handleReload()
+   }
+   async handleReload() {
+      const response = await api.main({ date: '' });
+      this.setState({ main: response });
+   }
     render() {
         return <div className='home'>
             <div className='title'>Recommended</div>
             <div className='gallery'>
-                <ImgBlock />
-                <ImgBlock />
-                <ImgBlock />
-                <ImgBlock />
-                <ImgBlock />
-                <ImgBlock />
+            {this.state.main.map((element) => <img src={element.image}></img>)}
                 {/*{imgs.map((img, index)=>{
                     return <img src={testImg} key={index} />
                 })}*/}
